697fe9d3840f6d59a2f4c26f533aa9b39531c707
kimminki <hungry87@hanmi.co.kr>
2023-02-15 16:30:39 +0900

    commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,155 @@
+﻿import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const cTotalSecond = 1500;
+  int totalSecond = cTotalSecond;
+  bool isRunning = false;
+  int totalPorodoros = 0;
+  late Timer timer;
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onStopPressed() {
+    if (totalSecond != cTotalSecond) {
+      setState(() {
+        timer.cancel();
+        isRunning = false;
+        totalSecond = cTotalSecond;
+      });
+    }
+  }
+
+  void onTick(Timer timer) {
+    if (totalSecond == 0) {
+      setState(() {
+        totalPorodoros = totalPorodoros + 1;
+        totalSecond = cTotalSecond;
+        isRunning = false;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSecond = totalSecond - 1;
+      });
+    }
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                format(totalSecond),
+                style: TextStyle(
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).cardColor,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: IconButton(
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: isRunning
+                        ? const Icon(Icons.pause_circle_outline)
+                        : const Icon(
+                            Icons.play_circle_outline,
+                          ),
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                  ),
+                ),
+                Center(
+                  child: IconButton(
+                    onPressed: onStopPressed,
+                    icon: const Icon(
+                      Icons.stop_circle_outlined,
+                    ),
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        ),
+                        Text(
+                          '$totalPorodoros',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
